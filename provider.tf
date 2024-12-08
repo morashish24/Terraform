288f5fa42e9c996880e3a5be945f933d43f79013
morashish24 <morashish24@gmail.com>
2024-12-09 02:48:47 +0530

    d

--- provider.tf
+++ provider.tf
@@ -1,5 +1,22 @@
-resource "aws_vpc" "custom_vpc" {
-
-    cidr_block = "10.0.0.0/16"
-
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
+
+
+
+
