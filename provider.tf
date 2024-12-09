01737410db07ef30b856703aabc174d8cb65c19f
morashish24 <morashish24@gmail.com>
2024-12-09 15:17:45 +0530

    d

--- provider.tf
+++ provider.tf
@@ -15,7 +15,7 @@ provider "aws" {
 # Create a VPC
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
-  enable_dns_support = true 
-  enable_dns_hostnames = true  
+  enable_dns_support = false
+  enable_dns_hostnames = false
 }
 
