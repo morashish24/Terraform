cd1786c5952b295853b160ce02a7f9466f9b5370
morashish24 <morashish24@gmail.com>
2024-12-10 13:28:12 +0530

    updated

--- provider.tf
+++ provider.tf
@@ -13,9 +13,23 @@ provider "aws" {
 }
 
 # Create a VPC
-resource "aws_vpc" "example" {
+resource "aws_vpc" "custom_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = false
   enable_dns_hostnames = false
 }
+
+
+variable "vpc_availability_zones" {
+  type = list (string)
+  description = "Availability Zones"
+  default = ["us-east-1a", "us-east-1b"]
+}
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.custom_vpc.id
+  count = length(var.vpc_availability_zones)
+  cidr_block = cidersubnet(aws_vpc.custom_vpc.cidr_block, 8, count.index+1)
+  }
+
+
 
