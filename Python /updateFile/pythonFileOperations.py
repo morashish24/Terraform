20932af9c01ecf6b0f987539fdd46c1b78bd41b5
morashish24 <morashish24@gmail.com>
2025-01-03 20:04:27 +0530

    d

--- Python /updateFile/pythonFileOperations.py
+++ Python /updateFile/pythonFileOperations.py
@@ -12,4 +12,29 @@
 
 # few function are in build and no need to do pip install or import it in our file like opening a file or closing a file 
 
-def update_server_config ()
+def update_server_config (file_path, key, value) :
+
+    with open ( file_path, 'r') as file :
+
+        lines = file.readlines()
+
+    with open ( file_path, 'w') as file :
+
+        for line in lines:
+
+            if key in lines :
+
+                file.write(key + "=" + value)
+
+            else :
+
+                file.write(line)
+
+# will call the function now 
+
+update_server_config("server.conf" , "MAX_CONNECTIONS", "1000")
+
+    
+
+
+             
