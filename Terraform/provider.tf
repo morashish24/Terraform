779706eaf8f44348749520f8af9a17f4cfc1bfdc
morashish24 <morashish24@gmail.com>
2025-01-16 02:01:26 +0530

    d

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -14,22 +14,22 @@ provider "aws" {
   region = "us-east-1"
 }
 # Create a VPC
-resource "aws_vpc" "custom_vpc" {
+/*resource "aws_vpc" "custom_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = false
   enable_dns_hostnames = false
-}
+}*/
 
 
 variable "vpc_availability_zones" {
   type = list (string)
-  description = "Availability Zones"
+  description = "Availability Zoneds"
   default = ["us-east-1a", "us-east-1b"]
 }
 
-resource "aws_subnet" "private_subnet" {
+/*resource "aws_subnet" "private_subnet" {
   vpc_id = aws_vpc.custom_vpc.id
-}
+}*/
 
 
 
