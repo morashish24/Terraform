42ffe7d17bdb2ba5e17dbc0839483dc7f0308f93
morashish24 <morashish24@gmail.com>
2025-01-16 21:51:33 +0530

    d

---
+++ Terraform/eks.tf
@@ -0,0 +1,24 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.27"
+  subnets         = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+
+      instance_type = "t3.medium"
+
+      key_name = "your-key-pair-name" # Replace with your EC2 key pair name
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Team        = "devops"d
+  }
+}
