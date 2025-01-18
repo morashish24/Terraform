81c324a23a865901de90e26b4725421553d42975
morashish24 <morashish24@gmail.com>
2025-01-18 21:31:50 +0530

    s

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -12,6 +12,12 @@ resource "aws_subnet" "subnet1"{
   availability_zone = "us-east-1a"
 }
 
+resource "aws_subnet" "subnet1"{
+  vpc_id = aws_vpc.main.id 
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
 resource "aws_internet_gateway" "DemoIGW" {
   vpc_id = aws_vpc.main.id
 
