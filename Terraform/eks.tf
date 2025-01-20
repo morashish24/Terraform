efc7bad8e00b03f348c73ff99b42459f6bcb7ea2
morashish24 <morashish24@gmail.com>
2025-01-20 21:23:39 +0530

    s

--- Terraform/eks.tf
+++ Terraform/eks.tf
@@ -3,7 +3,8 @@ module "eks" {
     cluster_name = "TerraformDemoCluster"
     cluster_version = "1.27"
 
-    subnet_ids = "aws_subnet.subnet1.id"
+    subnet_ids = ["aws_subnet.subnet1.id"]
+
 
     vpc_id = "aws_vpc.main.id"
 
