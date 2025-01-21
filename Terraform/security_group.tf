77e60301722b67f0fc1138e0c9de63b386d626fe
morashish24 <morashish24@gmail.com>
2025-01-21 22:38:17 +0530

    s

---
+++ Terraform/security_group.tf
@@ -0,0 +1,30 @@
+ /* resource "aws_security_group" "demo_sg" {
+    name        = "demo-security-group"
+    description = "Security group for example resources"
+    vpc_id      = aws_vpc.main.id # Replace with your VPC ID
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere
+    }
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"] # Allow HTTP traffic from anywhere
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1" # Allow all outbound traffic
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "example-sg"
+    }
+}*/
