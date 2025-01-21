8bf4764d9bb96ca5ac412fc09afe1128f85319dc
morashish24 <morashish24@gmail.com>
2025-01-21 22:24:42 +0530

    s

--- Terraform/eks.tf
+++ Terraform/eks.tf
@@ -17,10 +17,10 @@ module "eks" {
         }
     }
 
-    resource "aws_security_group" "example_sg" {
-    name        = "example-security-group"
+    resource "aws_security_group" "demo_sg" {
+    name        = "demo-security-group"
     description = "Security group for example resources"
-    vpc_id      = "vpc-12345678" # Replace with your VPC ID
+    vpc_id      = "aws_vpc.main.id" # Replace with your VPC ID
 
     ingress {
         from_port   = 22
