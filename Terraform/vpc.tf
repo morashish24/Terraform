1130b3355ba61666329e27a18f9c7591b8dc45df
morashish24 <morashish24@gmail.com>
2025-01-22 20:44:36 +0530

    s

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -76,4 +76,4 @@ resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
 resource "aws_iam_role_policy_attachment" "eks_container_registry_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.eks_node_role.name
-}
+} 
