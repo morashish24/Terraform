4a9909c3751ba0be586b0a4af0621784dc0b9983
morashish24 <morashish24@gmail.com>
2025-01-22 21:43:48 +0530

    s

--- Terraform/eks.tf
+++ Terraform/eks.tf
@@ -1,4 +1,4 @@
-module "eks" {
+/*module "eks" {
     source = "terraform-aws-modules/eks/aws" 
     cluster_name = "TerraformDemoCluster"
     cluster_version = "1.27"
@@ -23,5 +23,54 @@ module "eks" {
 
         
     }
+}*/
+
+
+# EKS Cluster
+resource "aws_eks_cluster" "main" {
+  name     = "DemoEksCluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = var.kubernetes_version
+
+  vpc_config {
+    subnet_ids = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+    endpoint_private_access = true
+    endpoint_public_access  = true
+    
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy,
+    module.vpc
+  ]
+
+  tags = {
+    Environment = var.environment
+  }
 }
 
+# EKS Node Group
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "demo-node-group"
+  node_role_arn   = aws_iam_role.node_group_role.arn
+  subnet_ids      = module.vpc.private_subnets
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = var.node_instance_types
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_group_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node_group_AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node_group_AmazonEC2ContainerRegistryReadOnly
+  ]
+
+  tags = {
+    Environment = var.environment
+  }
+}
--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -44,36 +44,3 @@ resource "aws_route_table_association" "demo_rt_asssociation" {
   subnet_id = aws_subnet.subnet1.id
   route_table_id = aws_route_table.demo_route_table.id
 }
-
-# EKS Node Group Role
-resource "aws_iam_role" "eks_node_role" {
-  name = "demo-node-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks_node_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks_node_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks_container_registry_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks_node_role.name
-} 
