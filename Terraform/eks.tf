5ba375a9d71212381e4a643b75dc242782a78c94
morashish24 <morashish24@gmail.com>
2025-01-22 20:53:31 +0530

    s

--- Terraform/eks.tf
+++ Terraform/eks.tf
@@ -20,6 +20,12 @@ module "eks" {
             desired_size = 2
             iam_role_arn = aws_iam_role.eks_node_role.arn 
         }
+
+        depends_on = [
+            aws_iam_role_policy_attachment.eks_worker_node_policy,
+            aws_iam_role_policy_attachment.eks_cni_policy,
+            aws_iam_role_policy_attachment.eks_container_registry_policy
+        ]
     }
 }
 
