7c0cb5a18e38b3e3c1b5e310f874d75b150f0215
morashish24 <morashish24@gmail.com>
2025-01-22 23:36:54 +0530

    s

--- Terraform/eks.tf
+++ Terraform/eks.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region = var.aws_region
-}
 
 # VPC Configuration
 module "vpc" {
