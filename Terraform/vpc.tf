1db123c2268fe4b57679c85609ae953091a7bd7a
morashish24 <morashish24@gmail.com>
2025-01-22 19:28:46 +0530

    s

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -9,12 +9,14 @@ resource "aws_vpc" "main" {
   vpc_id = aws_vpc.main.id 
   cidr_block = "10.0.0.0/24"
   availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "subnet2"{
   vpc_id = aws_vpc.main.id 
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_internet_gateway" "DemoIGW" {
